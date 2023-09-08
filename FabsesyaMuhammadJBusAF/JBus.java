0b25b499fa0a4fe02b468cef6fc1718651f59c22
unknown <fabsesyaputraibradi@gmail.com>
2023-09-08 11:35:51 +0700

    CS01 - Menyelesaikan CS

--- FabsesyaMuhammadJBusAF/JBus.java
+++ FabsesyaMuhammadJBusAF/JBus.java
@@ -1,35 +1,77 @@
 package FabsesyaMuhammadJBusAF;
 
-
-/**
- * Write a description of class JBus here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class JBus
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class JBus
-     */
-    public JBus()
-    {
-        // initialise instance variables
-        x = 1;
+    public static void main(String[] args){
+        JBus jBus = new JBus();
+        System.out.println("Bus ID: " + jBus.getBusId());
+        System.out.println("Bus Name: " + jBus.getBusName());
+        System.out.println("Is Discount: " + jBus.isDiscount());
+        System.out.println("Discount Percentage (1000, 900): " + jBus.getDiscountPercentage(1000, 900));
+        System.out.println("Discounted Price (1000, 10.0f): " + jBus.getDiscountedPrice(1000, 10.0f));
+        System.out.println("Original Price (900, 10.0f): " + jBus.getOriginalPrice(900, 10.0f));
+        System.out.println("Admin Fee Percentage: " + jBus.getAdminFeePercentage());
+        System.out.println("Admin Fee (1000): " + jBus.getAdminFee(1000));
+        System.out.println("Total Price (10000, 2): " + jBus.getTotalPrice(10000, 2));
+    } 
+    public static int getBusId(){
+        return 0;
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public static String getBusName(){
+        return "Bus";
     }
+    public static boolean isDiscount(){
+        return true;
+    }
+    public static float getDiscountPercentage(int beforeDiscount, int afterDiscount){
+        float discountPercentage;    
+        if(beforeDiscount>afterDiscount){
+            discountPercentage = ((beforeDiscount - afterDiscount)/beforeDiscount)*100;
+        }
+        else{
+            discountPercentage = 0.0f;
+        }
+        return discountPercentage;
+    }
+    public static int getDiscountedPrice(int price, float discountPercentage){
+        int discountedPrice = 0;
+        if( discountPercentage>100.0f){
+            discountPercentage=100.0f;
+        }
+        else{
+            discountedPrice = (int) (price - (price * (discountPercentage/100.0f)));
+        }
+        return discountedPrice;
+    }
+    
+    public static int getOriginalPrice(int discountedPrice, float discountPercentage){
+        int originalPrice;
+        if( discountPercentage>100.0f){
+            discountPercentage=100.0f;
+        }
+        originalPrice = (int) (discountedPrice/ (1-(discountPercentage/100.0f)));
+        return originalPrice;
+    }
+    
+    public static float getAdminFeePercentage(){
+        return 0.05f;
+    }
+    
+    public static int getAdminFee(int price){
+        float adminFeePercentage = getAdminFeePercentage();
+        int adminFee = (int)(price * adminFeePercentage);
+        return adminFee;
+    }
+    
+    public static int getTotalPrice(int price, int numberOfSeat){
+        int adminFee = getAdminFee(price * numberOfSeat);
+        return (price * numberOfSeat) + adminFee;
+    }
+    
+    
+    
+    
+    
+    
+    
 }
